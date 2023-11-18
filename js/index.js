a0937fba815a076f2f819fc25e11b37c14aae57b
Asilbek-17 <asilbekbozorbeyev@gmail.com>
2023-11-18 17:13:42 +0900

    Add back to buttons

--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ const elBox2 = document.querySelector('.box-2');
 const elBox3= document.querySelector('.box-3');
 const elPaginotion = document.querySelector('.container');
 const elInfoBox = document.querySelector('.info-box');
+const elBackTo2 = document.querySelector(".back-to-2");
+const elBackTo3 = document.querySelector(".back-to-3");
+const elBackTo4 = document.querySelector(".back-to-4");
+
 
 
 elButtons.forEach(btn => {
@@ -15,6 +19,26 @@ elButtons.forEach(btn => {
         elWrapper.appendChild(elBox)
         elBox.style.display = 'block'
     })
+})
+
+elBackTo2.addEventListener("click", ()=> {
+    elWrapper.innerHTML = ''
+    elButtons.forEach(btn => {
+        elWrapper.appendChild(btn)
+    })
+})
+
+elBackTo3.addEventListener("click", ()=> {
+    elWrapper.innerHTML = ''
+    elWrapper.appendChild(elBox)
+    elBox.style.display = 'block'
+})
+
+
+elBackTo4.addEventListener("click", ()=> {
+    elWrapper.innerHTML = '';
+    elWrapper.appendChild(elBox2);
+    elBox2.style.display = 'flex';
 })
 
 
